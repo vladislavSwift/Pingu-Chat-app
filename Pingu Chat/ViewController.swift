6315cfba9dd7affb8de821ac8573163ae0cb8de3
vladislavSwift <vazvladislavvaz@gmail.com>
2020-10-29 18:41:18 +0530

    Getting Started

---
+++ Pingu Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pingu Chat
+//
+//  Created by vladislav vaz on 29/10/20.
+//  Copyright © 2020 vladislav vaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
